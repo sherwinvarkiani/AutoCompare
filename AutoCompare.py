1137b7d3a0f44202f63bb1493ea848462433072f
sherwinvarkiani <s.varkiani@gmail.com>
2020-02-18 23:06:18 -0500

    Added text detection and started text comparison

--- AutoCompare.py
+++ AutoCompare.py
@@ -1,9 +1,12 @@
 import numpy as np 
 import cv2
-import json
+
 import boto3
 from botocore.exceptions import ClientError
 import logging
+
+# used for text differences
+import difflib
 
 # Get the images from the user
 # For now just get them from this directory
@@ -16,6 +19,57 @@ def processTextDetection(bucket, document):
 
     s3Object = s3Connection.Object(bucket, document)
     s3Response = s3Object.get()
+
+    # Detect the text in the image
+    client = boto3.client('textract')
+
+    # Process using S3 Object
+    response = client.detect_document_text(Document={'S3Object': {'Bucket' : bucket, 'Name' : document}})
+
+    # Get the text blocks
+    blocks = response['Blocks']
+    
+    textAttributes = {}
+    textList = []
+    textConfidence = []
+    textPolygon = []
+    # Go through all the text blocks
+    for block in blocks:
+        if block['BlockType'] == "WORD":
+            # If it detects a word add it to the dictionary and store the confidence
+            textList.append(block['Text'])
+            textConfidence.append("{:.2f}".format(block['Confidence']))
+            textPolygon.append('{}'.format(block['Geometry']['Polygon']))
+
+    textAttributes['Text'] = textList
+    textAttributes['Confidence'] = textConfidence
+    textAttributes['Polygon'] = textPolygon
+    return textAttributes
+
+def compareText(text1Attributes, text2Attributes):
+    print("Comparing Texts")
+    text1 = ""
+    text2 = ""
+
+    text1List = text1Attributes['Text']
+    text2List = text2Attributes['Text']
+    
+    # Get all of the text
+    for t in text1List:
+        text1 += t + " "
+
+    for t in text2List:
+        text2 += t + " "
+    
+    for i,s in enumerate(difflib.ndiff(text1,text2)):
+        if s[0] == ' ': continue
+        elif s[0] == '-':
+            # Delete (RED)
+            print(u'Delete "{}" from position {}'.format(s[-1],i))
+        elif s[0]=='+':
+            # Add (GREEN)
+            print(u'Add "{}" to position {}'.format(s[-1],i))  
+
 
 def uploadImage(imageName, bucket, objectName=None):
     #Upload the image to the s3 bucket
@@ -35,18 +89,32 @@ def uploadImage(imageName, bucket, objectName=None):
 
 def main():
     image1 = 'handwrittentext.png'
-    #image2 = 'image2.jpg'
+    image2 = 'image2.jpg'
 
+    # Read the images using opencv
     img1 = cv2.imread(image1, 1)
-    #img2 = cv2.imread(image2, 1)
+    img2 = cv2.imread(image2, 1)
 
+    # S3 Bucket name
     bucket = 'writing-compare-sherwinvarkiani'
-    uploadImage(image1, bucket)
 
+    # Upload the image to the bucket
+    uploadImage(image1, bucket)
     document = image1
+
+    # Use textract to get text from image
+    text1Attributes = processTextDetection(bucket, document)
+    print(text1Attributes)
+
+    # # Upload the image to the bucket
+    uploadImage(image2, bucket)
+    document = image2
+
+    # Use textract to get text from image
+    text2Attributes = processTextDetection(bucket, document)
+
+
     
-    block_count = processTextDetection(bucket, document)
-    print("Blocks detected: " + str(block_count))
 
 
 if __name__ == "__main__":
